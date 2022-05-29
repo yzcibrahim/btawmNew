5fef06e17dd6c697883ff4080d42ae6f3da9d509
ibrahim <yzcibrahim@gmail.com>
2022-05-29 14:06:39 +0300

    sonson

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,21 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CountContext } from "./Storage";
 
 export default function Home(){
 
     const [cnt,setCnt] = useContext(CountContext);
-    return <div onClick={()=>{setCnt(5)}}>HOme.....{cnt}</div>
+    const[deger,setDeger]=useState('asd')
+    //var asd="asd";
+
+    const degistir=()=>{
+        asd="değişti";
+        setDeger(asd);
+    }
+
+    return <div>
+        <div>var değişken:{asd}</div>
+        <button onClick={degistir}> değiştir</button>
+    </div>
+
+    // return <div onClick={()=>{setCnt(5)}}>HOme.....{cnt}</div>
 }
